18f7a6328dabef94d4070d3c734d3aa8abf59133
Pavel07-chyden <pates1993@mail.ru>
2022-01-30 20:52:47 +0300

    commit up data

--- src/App.tsx
+++ src/App.tsx
@@ -148,46 +148,4 @@ const App = () => {
 export default App;
 
 
-// import { countReset } from "console";
-// import React, { useEffect, useRef, useState } from "react";
-
-// const AppC=()=>{
-// function makeCounter() {
-//     let count = 0;
-//     return {
-//         count: function(){
-//             count ++
-//             console.log(count);}  }}
-//             var count = makeCounter()
-//             var coun2 = makeCounter()
-//             count.count() // 1
-//             count.count() // 2
-//             coun2.count() //1
-//             coun2.count() //2
-
-
-// var obj: any = {}
-// let p1 = "dimich"
-// let p2 = "sasha"
-
-// obj[p1] = 33
-// obj[p2] = 30 
-// obj[obj] = 100 
-// console.log(obj.p1); // undefined 
-// console.log(obj.dimich); // 33 
-// console.log(obj.sasha); // 30 
-
-// console.log(obj[p2]); //  30
-// return (
-//     <div>
-
-//     </div>
-// )
-// }
-
-// export default AppC;
-
-
-
-
 
